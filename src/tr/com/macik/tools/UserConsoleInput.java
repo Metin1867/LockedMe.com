f3a48760cf261178fbf387f5c542122a4114eb6e
Metin Acikalin <metin.dev21@hotmail.com>
2022-05-09 13:59:58 +0200

    Yes/No user input

--- src/tr/com/macik/tools/UserConsoleInput.java
+++ src/tr/com/macik/tools/UserConsoleInput.java
@@ -14,37 +14,44 @@ public class UserConsoleInput {
 	public static void main(String[] args) {
 		UserConsoleInput userInput = new UserConsoleInput();
 
-		HashMap<Integer, String> userInputMap = new HashMap<>();
-		userInputMap.put(1, "Texteingabe");
-		userInputMap.put(2, "Scannereingabe integer value");
-		userInputMap.put(3, "Fliesskommazahl");
-		userInputMap.put(4, "Ganze Zahl");
-		userInputMap.put(5, "Emailliste");
-		userInputMap.put(999, "Exit");
-		
-		int choice = new Integer(userInput.getChoice(userInputMap));
-		System.out.println("Auswahl: >" + choice + "<");
-		
-		switch (choice) {
-		case 1:	String s = userInput.getText("Eingabe ein Text");
-				System.out.println("Bestätigung Eingabe: >" + s + "<");
-				break;
-		case 2:	System.out.print("Eingabe direkt über Scanner: ");
-				int i = inpScanner.nextInt();
-				System.out.println("Bestätigung Eingabe: >" + i + "<");
-				break;
-		case 3:	Double d = userInput.getNumber("Eingabe Fliesskommazahl");
-				System.out.println("Bestätigung Eingabe: >" + d + "<");
-				break;
-		case 4:	Long w = userInput.getWholeNumber("Eingabe ganze Zahl");
-				System.out.println("Bestätigung Eingabe: >" + w + "<");
-				break;
-		case 5:	String[] emails = userInput.getLines("Email-Liste", ",");
-				for (String email : emails)
-					System.out.println(">" + email + "<");
-				break;
-		default:
-				;	// exit the main-methode
+		while (true) {
+			HashMap<Integer, String> userInputMap = new HashMap<>();
+			userInputMap.put(1, "Texteingabe");
+			userInputMap.put(2, "Scannereingabe integer value");
+			userInputMap.put(3, "Fliesskommazahl");
+			userInputMap.put(4, "Ganze Zahl");
+			userInputMap.put(5, "Emailliste");
+			userInputMap.put(6, "Yes/No");
+			userInputMap.put(999, "Exit");
+			
+			int choice = new Integer(userInput.getChoice(userInputMap));
+			System.out.println("Auswahl: >" + choice + "<");
+			
+			switch (choice) {
+			case 1:	String s = userInput.getText("Eingabe ein Text");
+					System.out.println("Bestätigung Eingabe: >" + s + "<");
+					break;
+			case 2:	System.out.print("Eingabe direkt über Scanner: ");
+					int i = inpScanner.nextInt();
+					System.out.println("Bestätigung Eingabe: >" + i + "<");
+					break;
+			case 3:	Double d = userInput.getNumber("Eingabe Fliesskommazahl");
+					System.out.println("Bestätigung Eingabe: >" + d + "<");
+					break;
+			case 4:	Long w = userInput.getWholeNumber("Eingabe ganze Zahl");
+					System.out.println("Bestätigung Eingabe: >" + w + "<");
+					break;
+			case 5:	String[] emails = userInput.getLines("Email-Liste", ",");
+					for (String email : emails)
+						System.out.println(">" + email + "<");
+					break;
+			case 6:	boolean b = userInput.getDecision("Denkst du positiv?");
+					System.out.println("Bestätigung Eingabe: >" + b + "<");
+					break;
+			default:
+					;	// exit the main-methode
+					System.exit(-1);
+			}
 		}
 	}
 
@@ -143,6 +150,17 @@ public class UserConsoleInput {
 			System.out.print(':');
 		}
 		return inpScanner.nextLine();
+	}
+
+	public boolean getDecision(String prompt) {
+		String input = "";
+		String[] list = {"Yes", "yes", "YES", "y", "Y", "No", "no", "NO", "n", "N"};
+		while (!isInList(input, list))
+			input = getText(prompt + " [Yes/No]");
+		if (input.toLowerCase().charAt(0) == 'y')
+			return true;
+					
+		return false;
 	}
 
 	/**
